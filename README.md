44bfa5cd465e425383a184e55104281da1269410
AlUlkesh <99896447+AlUlkesh@users.noreply.github.com>
2022-12-21 01:21:38 +0100

    Better file name generation
    File names now start with the number-prefix of the final image.

--- README.md
+++ README.md
@@ -12,3 +12,6 @@ The extension can be installed directly from within the **Extensions** tab withi
 You can also install it manually by running the following command from within the webui directory:
 
 	git clone https://github.com/AlUlkesh/sd_save_intermediate_images/ extensions/sd_save_intermediate_images
+
+## To-do
+Support DDIM and PLMS
--- scripts/sd_save_intermediate_images.py
+++ scripts/sd_save_intermediate_images.py
@@ -3,12 +3,12 @@ import os
 from modules import scripts
 from modules.processing import Processed, process_images, fix_seed
 from modules.sd_samplers import KDiffusionSampler, sample_to_image
-from modules.images import save_image
+from modules.images import save_image, FilenameGenerator, get_next_sequence_number
+from modules.shared import opts
 
 import gradio as gr
 
 orig_callback_state = KDiffusionSampler.callback_state
-
 
 class Script(scripts.Script):
     def title(self):
@@ -38,26 +38,49 @@ class Script(scripts.Script):
                     )
         return [is_active, intermediate_type, every_n]
 
-    def get_next_sequence_number(path):
-        from pathlib import Path
-        """
-        Determines and returns the next sequence number to use when saving an image in the specified directory.
-        The sequence starts at 0.
-        """
-        result = -1
-        dir = Path(path)
-        for file in dir.iterdir():
-            if not file.is_dir(): continue
-            try:
-                num = int(file.name)
-                if num > result: result = num
-            except ValueError:
-                pass
-        return result + 1
+    def save_image_only_get_name(image, path, basename, seed=None, prompt=None, extension='png', info=None, short_filename=False, no_prompt=False, grid=False, pnginfo_section_name='parameters', p=None, existing_info=None, forced_filename=None, suffix="", save_to_dirs=None):
+        #for description see modules.images.save_image
+        
+        namegen = FilenameGenerator(p, seed, prompt, image)
 
-    def run(self, p, is_active, intermediate_type, every_n):
-        fix_seed(p)
-        return Processed(p, images, p.seed)
+        if save_to_dirs is None:
+            save_to_dirs = (grid and opts.grid_save_to_dirs) or (not grid and opts.save_to_dirs and not no_prompt)
+
+        if save_to_dirs:
+            dirname = namegen.apply(opts.directories_filename_pattern or "[prompt_words]").lstrip(' ').rstrip('\\ /')
+            path = os.path.join(path, dirname)
+
+        os.makedirs(path, exist_ok=True)
+
+        if forced_filename is None:
+            if short_filename or seed is None:
+                file_decoration = ""
+            elif opts.save_to_dirs:
+                file_decoration = opts.samples_filename_pattern or "[seed]"
+            else:
+                file_decoration = opts.samples_filename_pattern or "[seed]-[prompt_spaces]"
+
+            add_number = opts.save_images_add_number or file_decoration == ''
+
+            if file_decoration != "" and add_number:
+                file_decoration = "-" + file_decoration
+
+            file_decoration = namegen.apply(file_decoration) + suffix
+
+            if add_number:
+                basecount = get_next_sequence_number(path, basename)
+                fullfn = None
+                for i in range(500):
+                    fn = f"{basecount + i:05}" if basename == '' else f"{basename}-{basecount + i:04}"
+                    fullfn = os.path.join(path, f"{fn}{file_decoration}.{extension}")
+                    if not os.path.exists(fullfn):
+                        break
+            else:
+                fullfn = os.path.join(path, f"{file_decoration}.{extension}")
+        else:
+            fullfn = os.path.join(path, f"{forced_filename}.{extension}")
+
+        return (fullfn)
 
     def process(self, p, is_active, intermediate_type, every_n):
         if is_active:
@@ -67,21 +90,33 @@ class Script(scripts.Script):
                 """
                 current_step = d["i"]
 
-                if current_step == 0:
-                    # Set custom folder for saving intermediates on first step
-                    intermed_path = os.path.join(p.outpath_samples, "intermediates")
-                    os.makedirs(intermed_path, exist_ok=True)
-                    intermed_number = Script.get_next_sequence_number(intermed_path)
-                    intermed_path = os.path.join(intermed_path, f"{intermed_number:05}")
-                    p.outpath_intermed = intermed_path
-
                 if current_step % every_n == 0:
                     if intermediate_type == "Denoised":
                         image = sample_to_image(d["denoised"])
                     else:
                         image = sample_to_image(d["x"])
 
-                    save_image(image, p.outpath_intermed, f"{current_step:03}", seed=int(p.seed), prompt=p.prompt, p=p)
+                    if current_step == 0:
+                        # Set custom folder for saving intermediates on first step
+                        intermed_path = os.path.join(p.outpath_samples, "intermediates")
+                        os.makedirs(intermed_path, exist_ok=True)
+                        # Set filename with pattern. Two versions depending on opts.save_images_add_number
+                        fullfn = Script.save_image_only_get_name(image, p.outpath_samples, "", int(p.seed), p.prompt, p=p)
+                        base_name = os.path.basename(fullfn)
+                        substrings = base_name.split('-')
+                        if opts.save_images_add_number:
+                            intermed_number = substrings[0]
+                            intermed_number = f"{intermed_number:05}"
+                            intermed_suffix = '-'.join(substrings[1:])
+                        else:
+                            intermed_number = get_next_sequence_number(intermed_path, "")
+                            intermed_number = f"{intermed_number:06}"
+                            intermed_suffix = '-'.join(substrings[0:])
+                        intermed_path = os.path.join(intermed_path, intermed_number)
+                        p.outpath_intermed = intermed_path
+                        p.outpath_intermed_pattern = intermed_number + "-%%%-" + intermed_suffix
+                    filename = p.outpath_intermed_pattern.replace("%%%", f"{current_step:03}")
+                    save_image(image, p.outpath_intermed, "", seed=int(p.seed), prompt=p.prompt, p=p, forced_filename=filename)
 
                 return orig_callback_state(self, d)
 
